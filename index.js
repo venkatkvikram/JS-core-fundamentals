3dcc7f53867f4ba55f5f8e8413dba8a3300ec093
VenkataSai268 <venkata.sai@axzorait.com>
2025-02-20 00:12:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let name1 = {
+  firstname: "venkat",
+  lastname: "Esampalli",
+  //   printFullName: function () {
+  //     console.log(this.firstname); //every fn has access to "this" keyword. Here this can access name object
+  //   },
+};
+
+let name2 = {
+  firstname: "Virat",
+  lastname: "Kohli",
+};
+
+let helloFullName = function printFullName(hometown, State) {
+  console.log(
+    this.firstname + " " + this.lastname + "," + hometown + "," + State
+  );
+};
+
+helloFullName.call(name1, "Godavarikhani", "Telanagana");
+helloFullName.apply(name2, ["Delhi", "Delhi"]);
+let boundFunction = helloFullName.bind(name1, "Godavrikhani, Telangana"); //returns a function
+boundFunction(); //gets callled 
+
