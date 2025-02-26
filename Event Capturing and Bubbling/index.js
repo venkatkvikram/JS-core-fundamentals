a18f05abf50ec82c19e7eaa0968462d29f57c1b9
VenkataSai268 <venkata.sai@axzorait.com>
2025-02-26 16:12:09 +0530

    Event Propogation Code

---
+++ Event Capturing and Bubbling/index.js
@@ -0,0 +1,11 @@
+document.querySelector('#grandParent').addEventListener("click", () => {
+    console.log("Grand Parent Clicked!");
+})
+
+document.querySelector('#parent').addEventListener("click", () => {
+    console.log("Parent Clicked!");
+}) 
+
+document.querySelector('#child').addEventListener("click", () => {
+    console.log("Child Clicked!");
+})
