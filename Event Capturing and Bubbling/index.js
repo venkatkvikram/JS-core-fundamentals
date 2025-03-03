9317f25975bd07df8815909285928ab53fdb162c
VenkataSai268 <venkata.sai@axzorait.com>
2025-03-03 11:02:00 +0530

    Prototype folder

--- Event Capturing and Bubbling/index.js
+++ Event Capturing and Bubbling/index.js
@@ -1,11 +1,12 @@
-document.querySelector('#grandParent').addEventListener("click", () => {
+document.querySelector('#grandParent').addEventListener("click", (e) => {
     console.log("Grand Parent Clicked!");
 })
 
-document.querySelector('#parent').addEventListener("click", () => {
+document.querySelector('#parent').addEventListener("click", (e) => {
     console.log("Parent Clicked!");
-}) 
+    // e.stopPropagation();
+},) 
 
-document.querySelector('#child').addEventListener("click", () => {
+document.querySelector('#child').addEventListener("click", (e) => {
     console.log("Child Clicked!");
 })
